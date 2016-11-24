ef53e4cc5297f8d64abca0e5189a8167b626007d
Tyler <tcameronsmith@gmail.com>
2016-11-24 16:58:25 -0500

    add class to 'td' when clicked, and will not allow td to be clicked again once selected

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,11 +2,29 @@ $(function() {
 	var turn = 0;
 
 	$('td').on('click', function() {
-		if (turn % 2 === 0) {
-			$(this).html('X');
+		if (!$(this).hasClass('x') && !$(this).hasClass('o')) {
+			if (turn % 2 === 0) {
+				$(this).html('X');
+				$(this).addClass('x');
+			} else {
+				$(this).html('O');
+				$(this).addClass('o');
+			}
+			turn++;
 		} else {
-			$(this).html('O');
 		}
-		turn++;
 	});
+
+
+
+	// if (('td .1').attr('X') === 'X') {
+	// 	console.log("it worked")
+	// }
 });
+
+
+// can't double click same box - click only if value is nil
+// win conditions
+// reset button
+// draw conditions
+// *text that says who's turn it is
